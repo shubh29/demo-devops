4af46c5e34b2d18cec862f63f105bb2df160f41d
masudhabib <masudhabib@users.noreply.github.com>
2018-08-24 21:41:12 -0400

    baseline for spring boot test app
    what else can be said

---
+++ src/main/java/org/learn/devops/demodevops/DemoDevopsApplication.java
@@ -0,0 +1,12 @@
+package org.learn.devops.demodevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDevopsApplication.class, args);
+	}
+}
---
+++ src/test/java/org/learn/devops/demodevops/DemoDevopsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.learn.devops.demodevops;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoDevopsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
